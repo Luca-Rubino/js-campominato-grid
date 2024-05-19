d94857491b304fe21f13c29086844b7fcc2c94b4
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-19 20:01:40 +0200

    Base scaffolding

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log('Js-campominato-grid')
+
+
+// inserisco il titolo dell'esercizio attraverso il js
+const bodySelector = document.querySelector('body')
+const section = document.createElement('section')
+const h1 = document.createElement('h1')
+bodySelector.appendChild(section)
+section.appendChild(h1)
+h1.textContent='Js-campominato-grid'
