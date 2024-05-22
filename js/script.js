b073d589ad2a1c0e9a4824337985118a1788a620
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-22 21:06:55 +0200

    Inserire funzione per attivare la griglia al click del bottone, correzione dello style.css con cancellazione del border dalla classe griglia e correzione del width, tentativo non riuscito di colorare i quadrati della griglia al click

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 console.log('Js-campominato-grid')
 
-
-// inserisco il titolo dell'esercizio attraverso il js
-const bodySelector = document.querySelector('body')
-const section = document.createElement('section')
-const h1 = document.createElement('h1')
-bodySelector.appendChild(section)
-section.appendChild(h1)
-h1.textContent='Js-campominato-grid'
+// esercizio
 
 // inizio esercizio
 
+const bodySelector = document.querySelector('body')
 const sectionSecondo = document.createElement('section')// creazione sezione per inserire i quadrati numerati
 sectionSecondo.classList.add('griglia')// aggiungo classe griglia per la personalizzazione con il file css
 
-let elQuadrato
 
-// ciclo per creare i quadrati da 1 a 100
-for (let i = 0; i < 100; i++){
-    bodySelector.appendChild(sectionSecondo)// inserisco nel body la sezione dei quadrati
-    elQuadrato = document.createElement('article')// creo l'elemento article che sarà il quadrato numerato 
-    elQuadrato.classList.add('quadrato')// aggiungo la classe per la personalizzazione con il file css
-    sectionSecondo.appendChild(elQuadrato)// aggiungo come figlio di section i vari article
-    elQuadrato.append(i+1)// inserisco in ogni singolo article il valore numerico
-}
+let elQuadrato, i
+
+const buttonPlay = document.querySelector('button')
+buttonPlay.addEventListener("click", playGame)
+function playGame(){
+    // ciclo per creare i quadrati da 1 a 100
+    for (i = 0; i < 100; i++){
+        bodySelector.appendChild(sectionSecondo)// inserisco nel body la sezione dei quadrati
+        elQuadrato = document.createElement('article')// creo l'elemento article che sarà il quadrato numerato 
+        elQuadrato.classList.add('quadrato')// aggiungo la classe per la personalizzazione con il file css
+        sectionSecondo.appendChild(elQuadrato)// aggiungo come figlio di section i vari article
+        elQuadrato.addEventListener("click", sfondoCeleste)
+        function sfondoCeleste() {
+            elQuadrato.classList.add('bk-celeste')
+            elQuadrato.append()// inserisco in ogni singolo article il valore numerico
+        }}};
