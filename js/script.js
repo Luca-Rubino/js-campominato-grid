545613fc1dc2788b6372c39e9b9867c90a0a93af
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-25 18:44:27 +0200

    Creare funzione che genera la griglia richiesta al click del pulsante button ed all'interno della stessa uso l'espressione switch per impostare la generazione delle celle in base alla difficoltà selezionata

--- js/script.js
+++ js/script.js
@@ -17,3 +17,25 @@ const bodySelector = document.querySelector('body');// seleziono il tag body nel
 const sectionSecondo = document.createElement('section');// creazione sezione per inserire i quadrati numerati
 sectionSecondo.classList.add('griglia');// aggiungo classe griglia per la personalizzazione con il file css
 bodySelector.appendChild(sectionSecondo);// inserisco nel body la sezione dei quadrati
+
+// funzione che genera la griglia richiesta
+buttonPlay.addEventListener("click", () => {
+    const livello = modalitaDiGioco.value; // costante per richiamare i value dei tag option per le difficolta
+    console.log(livello);
+    // uso l'espressione switch per impostare la generazione delle celle in base alla difficoltà selezionata 
+    switch (livello) {
+        case '0':
+            numCelle = 100;
+            larghezzaRem = 3.5;
+            break;
+        case '1':
+            numCelle = 81;
+            larghezzaRem = 3.7;
+            break;
+        case '2':
+             numCelle = 49;
+             larghezzaRem = 5;       
+    }
+    console.log(numCelle);
+    console.log(larghezzaRem);
+});
